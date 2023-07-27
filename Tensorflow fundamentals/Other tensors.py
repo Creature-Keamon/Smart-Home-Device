ffa738c36f65732abaa74aba0329ec0853ffbbcf
Shawn <Shuttlez4@outlook.com>
2023-07-27 20:55:36 +1200

    more tensorflow shenanigans

--- Tensorflow fundamentals/Other tensors.py
+++ Tensorflow fundamentals/Other tensors.py
@@ -19,3 +19,22 @@ print("shape of tensor", rank_4_tensor.shape)
 print("elements along the 0 axis", rank_4_tensor.shape[0])
 print("elements along the last axis", rank_4_tensor.shape[-1])
 print("total number of elements", tf.size(rank_4_tensor).numpy())
+
+#indexing tensors
+
+rank_4_tensor[:2, :2, :2, :2]
+
+rank_4_tensor[:2, :2, :, :2]
+
+rank_2_tensor = tf.constant(shape=[[2, 3], [4, 5]])
+
+#get last item of each rank 2 tensor
+rank_2_tensor[:, -1]
+
+#add an extra dimension to the tensor making it larger
+rank_3_tensor = rank_2_tensor[..., tf.newaxis]
+
+#alternative
+
+tf.expand_dims(rank_2_tensor, axis=-1)
+
