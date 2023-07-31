ab24843148f83572c19da64b518d1cd541e53cb4
Shawn <Shuttlez4@outlook.com>
2023-07-31 20:47:43 +1200

    regression shenanigans

--- Tensorflow fundamentals/Neural Networks Regression.py
+++ Tensorflow fundamentals/Neural Networks Regression.py
@@ -7,26 +7,32 @@ import matplotlib.pyplot as plt
 #creating scalar features (input factors affecting the outcome)
 X = np.array([-7.0, -4.0, -1.0, 2.0, 5.0, 8.0, 11.0, 14.0])
 
-#create labels
+#create labels (Y axis)
 y = np.array([3.0, 6.0, 9.0, 12.0, 15.0, 18.0, 21.0, 24.0])
 
-#visualise the graph as a scatter graph
-plt.scatter(X,y);
-plt.show()
-
-
-#demo
-house_info = tf.constant(["bedroom", "bathroom", "garage"])
-house_price = tf.constant([939570])
-
-#checking shape
-input_shape = X[0].shape
-
-X[0].ndim # check dimensions
-
 #converting np array into tensor
-Xtensor = tf.constant(X)
-ytensor = tf.constant(y)
+Xtensor = tf.cast(tf.constant(X), dtype=tf.float32)
+ytensor = tf.cast(tf.constant(y), dtype=tf.float32)
 
 input_shape = X[0].shape
 output_shape = y[0].shape
+
+#create a seed
+tf.random.set_seed(42)
+
+#Step 1. creating a model
+model = tf.keras.Sequential([ #"provides training and inference features for the model"
+    tf.keras.layers.Dense(1)
+    ])
+
+#Step 2. Compile the model
+model.compile(loss=tf.keras.losses.mae, #mae = mean absolute error
+              optimizer=tf.keras.optimizers.SGD(), #SGD = stochastic gradient descent (tells model how it should improve)
+              metrics=["mae"])
+
+#Step 3. fit the model
+model.fit(tf.expand_dims(X, axis=-1), y, epochs=400) #makes model figures out the relationship between X and y
+
+#make a prediction with the model (what value of y does it think will be when X is 17)
+y_prediction = model.predict([17.0])
+print(y_prediction)
